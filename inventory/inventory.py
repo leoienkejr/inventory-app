e94e3da94b973d43bf195f9bc99db924e2e1b45e
leoienkejr <leonardoienkejr@gmail.com>
2018-02-02 19:53:29 -0200

    1. Renamed inventory_lib.py to lib.py  2. Implemented initialization code in inventory.py

--- inventory/inventory.py
+++ inventory/inventory.py
@@ -6,6 +6,16 @@ inventory.py
 Inventory management command-line application.
 
 '''
+import os
+import appdirs
+import lib
+
+APPNAME = 'inventory-app'
+DB_FILENAME = 'inventory.db'
 
 if __name__ == '__main__':
-    pass
+    db_path = appdirs.user_data_dir(appname=APPNAME, appauthor=False)
+    if not os.path.exists(db_path):
+        os.makedirs(db_path)
+    db_path = os.path.join(db_path, DB_FILENAME)
+    db = lib.Inventory(db_path)
